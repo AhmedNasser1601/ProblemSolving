853edb9257df1c28833b1d31d971d5df7ef5d7e5
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 01:08:31 +0200

    Check Order

---
+++ Training/Training/Order check.cpp
@@ -0,0 +1,24 @@
+////Order check
+//#include<iostream>
+//using namespace std;
+//bool is_in_order(int a, int b, int c)                   //Definition
+//{
+//	if (a <= b &&b <= c)
+//		return true;
+//	else
+//		return false;
+//}
+//void main()
+//{
+//	int num1, num2, num3;
+//	while (true)
+//	{
+//		cout << "\n\nEnter Three Numbers\n";
+//		cin >> num1 >> num2 >> num3;
+//		bool check = is_in_order(num1, num2, num3);     //Calling
+//		if (check)
+//			cout << "In Order\n";
+//		else
+//			cout << "Not In Order\n";
+//	}
+//}
