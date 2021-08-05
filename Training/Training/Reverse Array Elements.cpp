d603841153ade6155e74549ef3baa365b0a0679a
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 01:11:25 +0200

    Reverse Array Elements

---
+++ Training/Training/Reverse Array Elements.cpp
@@ -0,0 +1,23 @@
+////Reverse Array Elements
+//#include<iostream>
+//using namespace std;
+//#define size 5
+//void reverse(int old_list[], int new_list[])
+//{
+//	for (int i = 0, j = size - 1; i < size; i++)
+//		new_list[j--] = old_list[i];
+//}
+//void Display(int Arr[])
+//{
+//	for (int k = 0; k < size; k++)
+//		cout << Arr[k] << "\t\n";
+//}
+//int main()
+//{
+//	int list1[size] = { 41, 22, 64, 75, 36 };
+//	int list2[size];
+//	reverse(list1, list2);
+//	Display(list2);
+//	system("pause");
+//	return 0;
+//}
