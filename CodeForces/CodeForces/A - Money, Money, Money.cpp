598469ef17745fac34d2eda036340e219d8e6bb5
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-05 23:33:44 +0200

    Week 1: Practice, problem: (A) Money, Money, Money!

---
+++ CodeForces/CodeForces/A - Money, Money, Money.cpp
@@ -0,0 +1,15 @@
+//#include<iostream>
+//
+//using namespace std;
+//
+//int main() {
+//	int n, x, y;
+//
+//	cin >> n >> x >> y;
+//
+//	if (n >= 1 && x >= 1 && y >= 1 && n <= 100000 && x <= 100000 && y <= 100000 && x <= y) {
+//		cout << ((n * y) - (n * x)) << endl;
+//	}
+//
+//	return 0;
+//}
