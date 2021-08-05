bac1f92918725f99331a31ddb5d6c816e894843d
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-05 23:19:55 +0200

    Week 1: Practice, problem: (B) Boo and The Equation

---
+++ CodeForces/CodeForces/B - Boo and The Equation.cpp
@@ -0,0 +1,14 @@
+//#include<iostream>
+//#include<iomanip>
+//
+//using namespace std;
+//
+//int main() {
+//	double a, b, c;
+//
+//	cin >> a >> b >> c;
+//
+//	cout << fixed << setprecision(6) << (((a * 2) + (b * 3)) * (c * 5)) << endl;
+//
+//	return 0;
+//}
