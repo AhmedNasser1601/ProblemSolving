b258e2259f9d12bdb34f1202103d520f13585a9d
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 01:08:59 +0200

    Password Validation

---
+++ Training/Training/Password validation.cpp
@@ -0,0 +1,22 @@
+////Password Validation Example
+//#include<iostream>
+//#include<conio.h>   // ---> Password ---> [Invisible] ---> On Console
+//using namespace std;
+//int main()
+//{
+//	char ch; bool is_special = false, is_num = false;
+//	cout << "Enter Password...";
+//	while (true)
+//	{
+//		ch = _getch();
+//		if (ch == '\r')   // ---> Button ---> [Enter] ---> Is Break
+//			break;
+//		if (ch == '$' || ch == '@' || ch == '#' || ch == '_' || ch == '%' || ch == '&')
+//			is_special = true;
+//		if (ch >= 48 && ch <= 57)
+//			is_num = true;
+//		if (is_special && is_num)
+//			cout << "\n\tPassword is Valid\n";
+//	}
+//	system("pause"); return 0;
+//}
