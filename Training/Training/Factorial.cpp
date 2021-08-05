b164ea6bed606c7f82f97eaf0183a94b0c009660
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 01:27:02 +0200

    Factorial

--- Training/Training/Factorial.cpp
+++ Training/Training/Factorial.cpp
@@ -1,22 +1,22 @@
-#include <iostream>
-using namespace std;
-void factors(int num)
-{
-	cout << "Factors of " << num << " are: " << endl;
-	for (int i = 1; i <= num; ++i)
-	{
-		if (num % i == 0)
-			cout << "--->" << i << "\n";
-	}
-}
-int main()
-{
-	int n;
-	while (true)
-	{
-		cout << "\nEnter a positive integer: ";
-		cin >> n;
-		factors(n);
-	}
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//void factors(int num)
+//{
+//	cout << "Factors of " << num << " are: " << endl;
+//	for (int i = 1; i <= num; ++i)
+//	{
+//		if (num % i == 0)
+//			cout << "--->" << i << "\n";
+//	}
+//}
+//int main()
+//{
+//	int n;
+//	while (true)
+//	{
+//		cout << "\nEnter a positive integer: ";
+//		cin >> n;
+//		factors(n);
+//	}
+//	return 0;
+//}
