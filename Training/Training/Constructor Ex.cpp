1028afc5c476cc203825f2ade7737815bf466cfc
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 01:03:33 +0200

    Constructor example

---
+++ Training/Training/Constructor Ex.cpp
@@ -0,0 +1,56 @@
+//#include<iostream>
+//#include<string>
+//
+//using namespace std;
+//
+//class triangle
+//{
+//private:
+//	int base;
+//	int height;
+//
+//public:
+//	triangle() //Empty Constructor
+//	{
+//		cout << "\tEmpty Constructor\n";
+//		base = 0;
+//		height = 0;
+//	}
+//
+//	void setBase_height(int b, int h)
+//	{
+//		base = b;
+//		height = h;
+//	}
+//
+//	void print()
+//	{
+//		cout
+//			<< endl
+//			<< "base : " << base
+//			<< "\t"
+//			<< "height : " << height
+//			<< endl << endl;
+//	}
+//};
+//
+//int main()
+//{
+//	triangle t1, t2, t3, t4;
+//
+//	t1.setBase_height(1, 2);
+//	t2.setBase_height(3, 4);
+//	t3.setBase_height(5, 6);
+//
+//	cout << endl << endl;
+//
+//	t1.print();
+//	t2.print();
+//	t3.print();
+//	t4.print();
+//
+//	cout << endl << endl;
+//
+//	system("pause");
+//	return 0;
+//}
