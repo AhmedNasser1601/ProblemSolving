e852575e8baa05dcd9b589eae7e834a60bb21383
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 01:06:22 +0200

    Files example

---
+++ Training/Training/Files Ex.cpp
@@ -0,0 +1,23 @@
+//#include<iostream>
+//#include<fstream>
+//using namespace std;
+//int main()
+//{
+//	ofstream file("Ahmed.txt");
+//	cout << "Enter Name & ID\n";
+//	cout << "Press [ctrl+z] to exit\n";
+//	char name[10];
+//	int phone;
+//	int counter = 1;
+//	while (cin >> name >> phone)
+//	{
+//		file
+//			<< "no." << counter << ": "
+//			<< name << " -> " << phone << "\n";
+//		counter = counter + 1;
+//	}
+//	file.close();
+//
+//	system("pause");
+//	return 0;
+//}
