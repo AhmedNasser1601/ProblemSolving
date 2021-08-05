28eb789ebf873fe32527f4109e124e2dc14fcbe2
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 01:13:36 +0200

    function Swap

---
+++ Training/Training/Swap function.cpp
@@ -0,0 +1,22 @@
+////Function Swaps 2 num (mathematically)
+//#include <iostream>
+//using namespace std;
+//void my_swap(int&, int&);
+//int main()
+//{
+//	int n1, n2;
+//	cout<<"Enter two numbers:\n";
+//	cout << "--->"; cin >> n1;
+//	cout << "--->"; cin >> n2;
+//	cout<<n1<<"\t"<<n2<<endl;
+//	my_swap(n1, n2);
+//	cout<<n1<<"\t"<<n2<<endl;
+//	system("pause");
+//	return 0;
+//}
+//void my_swap(int& a, int& b)
+//{
+//	a += b;
+//	b = a - b;
+//	a -= b;
+//}
