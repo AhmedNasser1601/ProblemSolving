955cee5ee99a79c3f7821bc3873ed42775977b64
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 01:08:46 +0200

    Passing py Reference

---
+++ Training/Training/Pass by Ref.cpp
@@ -0,0 +1,20 @@
+////Passing by Reference Ex
+//#include<iostream>
+//using namespace std;
+//void swap(int& a, int& b)
+//{
+//	int temp;
+//	temp = a;
+//	a = b;
+//	b = temp;
+//}
+//int main()
+//{
+//	int n = 0, m = 0;
+//	cout << "Enter two numbers to swap\n";
+//	cin >> n >> m;
+//	swap(n, m);
+//	cout << "\t" << n << "\t" << m << endl;
+//	system("pause");
+//	return 0;
+//}
