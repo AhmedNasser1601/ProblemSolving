7a89a2011aef10988a0a3354fc3faee0b74ba950
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-05 23:07:07 +0200

    Week 2: Practice, problem: (A) Yan and Maths, Accepted

---
+++ CodeForces/CodeForces/A - Yan and Maths.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+stack<int> numSplit;
+
+void massSplit(long long num) {
+	int temp;
+
+	while (num != 0) {
+		temp = num % 10;
+		num /= 10;
+		numSplit.push(temp);
+	}
+}
+
+void checkEvenOdd(long long& x, long long& y) {
+	while (!numSplit.empty()) {
+		if (numSplit.top() % 2 == 0) x++;
+		else y++;
+
+		numSplit.pop();
+	}
+}
+
+int main() {
+	long long x = 0, y = 0, z;
+	cin >> z;
+
+	massSplit(z);
+	checkEvenOdd(x, y);
+
+	cout << "Even : " << x << "\nOdd : " << y << endl;
+
+	return 0;
+}
