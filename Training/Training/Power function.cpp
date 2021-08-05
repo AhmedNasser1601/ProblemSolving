515e6f1fd8bd750d4d89ac4c2a4756cbbf06f3db
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 01:09:09 +0200

    function Power

---
+++ Training/Training/Power function.cpp
@@ -0,0 +1,22 @@
+////Power function
+//#include<iostream>
+//using namespace std;
+//int POWER(int, int);
+//int main()
+//{
+//	while (true)
+//	{
+//		int base, exponent;
+//		cout << "\n\nEnter the Base Number..\t"; cin >> base;
+//		cout << "Enter the Exponential..\t"; cin >> exponent;
+//		int Result_Pow = POWER(base, exponent);
+//		cout << "\nThe Result of " << base << "^" << exponent << " is: " << Result_Pow << endl;
+//	}
+//}
+//int POWER(int Base, int Exponent)
+//{
+//	int Result = 1;
+//	for (int i = 1; i <= Exponent; i++)
+//		Result *= Base;
+//	return Result;
+//}
