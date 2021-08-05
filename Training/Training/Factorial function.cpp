102a2f507d92f554c3535c9dd534d4ced0f3ec2b
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 01:05:32 +0200

    function Factorial

---
+++ Training/Training/Factorial function.cpp
@@ -0,0 +1,23 @@
+////Factorial function
+//#include<iostream>
+//using namespace std;
+//void Factorial(int number)
+//{
+//	long long factorial = 1;
+//	for (int i = 1; i <= number; i++)
+//		factorial *= i;
+//	cout << "\tFactorial of << " << number << " >> is ---> ";
+//	cout << factorial << endl;
+//	cout << "*********************************************\n";
+//}
+//int main()
+//{
+//	cout << endl;
+//	cout << "*********************************************\n";
+//	while (true)
+//	{
+//		cout << "\tEnter your number:\t"; long long number;
+//		cin >> number;
+//		Factorial(number);
+//	}
+//}
