098975e5637e12cb982348ee6efe0f7b7d31d5c9
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 01:02:06 +0200

    Area of Circle

---
+++ Training/Training/Area of Circle.cpp
@@ -0,0 +1,13 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	float R, n = 3.14159;
+//	while (true)
+//	{
+//		cin >> R;
+//		cout << "A=" << n*R*R << endl << endl;
+//	}
+//	system("pause");
+//	return 0;
+//}
