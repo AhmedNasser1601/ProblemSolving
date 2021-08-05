450ab1c048911ea2a172d77cc423cf3a36b9fd06
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 01:08:18 +0200

    Multiply number

---
+++ Training/Training/Multiply Num.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+#define size 10
+void input(int arr[], int multiply_num)
+{
+	cout << "Enter (" << size << ") Numbers..\n";
+	for (int i = 0; i < size; i++)
+	{
+		cout << "\t--(" << i+1 << ")--> ";
+		cin >> arr[i];
+	}
+	cout << "\n\n";
+}
+void duplicate(int arr[], int multiply_num)
+{
+	for (int i = 0; i < size; i++)
+		arr[i] = arr[i] * multiply_num;
+}
+void display(int arr[])
+{
+	for (int i = 0; i < size; i++)
+		cout << "\t" << arr[i] << "\n\n\n";
+}
+int main()
+{
+	cout << "\nEnter The Multiply Number:";
+	int multiply_num = 0;
+	cout << " ---> "; cin >> multiply_num;
+	int arr[size] = {};
+	input(arr, multiply_num);
+	cout << "----->\t";
+	display(arr);
+	for (int i = 0; i < size; i++)
+	{
+		duplicate(arr, multiply_num);
+		display(arr);
+	}
+	system("pause");
+	return 0;
+}
