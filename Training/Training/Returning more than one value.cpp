9a95c94adf3f41778a5450abc3faa4d313444216
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 01:10:44 +0200

    Return more than one value example

---
+++ Training/Training/Returning more than one value.cpp
@@ -0,0 +1,18 @@
+////Returning more than one value
+////Passing by reference && Passing by value Ex
+//#include<iostream>
+//using namespace std;
+//void get_num(int& x, int y, int& z)
+//{
+//	cout << x << "\t" << y << "\t" << z << endl;
+//	x = 1; y = 2; z = 3;
+//	cout << x << "\t" << y << "\t" << z << endl;
+//}
+//int main()
+//{
+//	int a = 10, b = 20, c = 30;
+//	get_num(a, b, c);
+//	cout << a << "\t" << b << "\t" << c << endl;
+//	system("pause");
+//	return 0;
+//}
