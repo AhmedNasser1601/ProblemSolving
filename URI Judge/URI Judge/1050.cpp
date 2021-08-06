01f225b9d097eb360bbaacd88abe7c766026d32b
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:32:59 +0200

    DDD

---
+++ URI Judge/URI Judge/1050.cpp
@@ -0,0 +1,25 @@
+//#include <iostream>
+//#include <stdio.h>
+//#include <math.h>
+//#include <iomanip>
+//#include <string>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int num[8] = { 61, 71, 11, 21, 32, 19, 27, 31 };
+//	string str[8] = { "Brasilia", "Salvador", "Sao Paulo", "Rio de Janeiro", "Juiz de Fora", "Campinas", "Vitoria", "Belo Horizonte" };
+//
+//	int input;
+//	cin >> input;
+//
+//	for (int i = 0; i < 8; i++)
+//		if (input == num[i])
+//			cout << str[i] << endl;
+//
+//	if (input != num[0] && input != num[1] && input != num[2] && input != num[3] && input != num[4] && input != num[5] && input != num[6] && input != num[7] && input != num[8])
+//		cout << "DDD nao cadastrado\n";
+//
+//	return 0;
+//}
