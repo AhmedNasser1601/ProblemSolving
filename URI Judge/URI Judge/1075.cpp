ccf2922481f4c324649dcd9901c394d66230ed88
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:36:03 +0200

    Remaining 2

---
+++ URI Judge/URI Judge/1075.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//#include <iomanip>
+//#include <stdio.h>
+//#include <string>
+//#include <math.h>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int N = 0;
+//	cin >> N;
+//
+//	for (int i = 0; i < 10000; i++)
+//	{
+//		if (i % N == 2)
+//			cout << i << endl;
+//	}
+//
+//	return 0;
+//}
