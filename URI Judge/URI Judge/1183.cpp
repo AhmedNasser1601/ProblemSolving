0c01f39b283aca22659c33605c200c8309f67469
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:56:06 +0200

    Above the Main Diagonal

---
+++ URI Judge/URI Judge/1183.cpp
@@ -0,0 +1,40 @@
+//#include <iostream>
+//#include <iomanip>
+//#include <string>
+//#include <cstdio>
+//#include <cmath>
+//
+//using namespace std;
+//
+//int main() {
+//	char x = ' ';
+//	float y[12][12] = {}, sum = 0, avg = 0;
+//
+//	cin >> x;
+//
+//	for (int i = 0; i < 12; i++) {
+//		for (int j = 0; j < 12; j++) {
+//			cin >> y[i][j];
+//		}
+//	}
+//
+//	switch (x) {
+//	case 'S':
+//		for (int i = 0; i < 12; i++)
+//			for (int j = 0; j < 12; j++)
+//				if (i != j && j > i)
+//					sum += y[i][j];
+//		cout << fixed << setprecision(1) << sum << endl;
+//		break;
+//
+//	case 'M':
+//		for (int i = 0; i < 12; i++)
+//			for (int j = 0; j < 12; j++)
+//				if (i != j && j > i)
+//					avg += y[i][j];
+//		cout << fixed << setprecision(1) << avg / 66 << endl;
+//		break;
+//	}
+//
+//	return 0;
+//}
