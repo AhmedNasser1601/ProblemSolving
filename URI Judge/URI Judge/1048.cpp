e2c9a95dff5a60b6014e79b11367409534b6ee8e
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:32:17 +0200

    Salary Increase

---
+++ URI Judge/URI Judge/1048.cpp
@@ -0,0 +1,64 @@
+//#include <iostream>
+//#include <stdio.h>
+//#include <math.h>
+//#include <iomanip>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	float x, y, z;
+//	cin >> x;
+//
+//	if (x > 0 && x <= 400)
+//	{
+//		y = 0;
+//		y = x + (x * 0.15);
+//		z = y - x;
+//		cout << "Novo salario: " << fixed << setprecision(2) << y << endl;
+//		cout << "Reajuste ganho: " << fixed << setprecision(2) << z << endl;
+//		cout << "Em percentual: 15 %" << endl;
+//	}
+//
+//	else if (x > 400.01 && x <= 800)
+//	{
+//		y = 0;
+//		y = x + (x * 0.12);
+//		z = y - x;
+//		cout << "Novo salario: " << fixed << setprecision(2) << y << endl;
+//		cout << "Reajuste ganho: " << fixed << setprecision(2) << z << endl;
+//		cout << "Em percentual: 12 %" << endl;
+//	}
+//
+//	else if (x > 800.01 && x <= 1200)
+//	{
+//		y = 0;
+//		y = x + (x * 0.10);
+//		z = y - x;
+//		cout << "Novo salario: " << fixed << setprecision(2) << y << endl;
+//		cout << "Reajuste ganho: " << fixed << setprecision(2) << z << endl;
+//		cout << "Em percentual: 10 %" << endl;
+//	}
+//
+//	else if (x > 1200.01 && x <= 2000)
+//	{
+//		y = 0;
+//		y = x + (x * 0.07);
+//		z = y - x;
+//		cout << "Novo salario: " << fixed << setprecision(2) << y << endl;
+//		cout << "Reajuste ganho: " << fixed << setprecision(2) << z << endl;
+//		cout << "Em percentual: 7 %" << endl;
+//	}
+//
+//	else if (x > 2000)
+//	{
+//		y = 0;
+//		y = x + (x * 0.04);
+//		z = y - x;
+//		cout << "Novo salario: " << fixed << setprecision(2) << y << endl;
+//		cout << "Reajuste ganho: " << fixed << setprecision(2) << z << endl;
+//		cout << "Em percentual: 4 %" << endl;
+//	}
+//
+//	return 0;
+//}
