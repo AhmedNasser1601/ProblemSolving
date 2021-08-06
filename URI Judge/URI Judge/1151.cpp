be45367ed45736dd7de3a674819ae194fe1a66f8
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:50:50 +0200

    Easy Fibonacci

---
+++ URI Judge/URI Judge/1151.cpp
@@ -0,0 +1,30 @@
+//#include <iostream>
+//#include <stdio.h>
+//#include <iomanip>
+//#include <string>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int x, arr[100] = { 0,1 };
+//	cin >> x;
+//
+//	if (x > 0 && x < 46)
+//	{
+//		for (int i = 0; i < x; i++)
+//		{
+//			arr[i + 2] = arr[i + 1] + arr[i];
+//
+//			cout << arr[i];
+//
+//			if (i == x - 1)
+//				cout << endl;
+//			else
+//				cout << " ";
+//		}
+//
+//	}
+//
+//	return 0;
+//}
