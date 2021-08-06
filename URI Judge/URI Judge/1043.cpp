ef0ffba9e3110c1690bf3e0a71fd48db63267da7
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:31:23 +0200

    Triangle

---
+++ URI Judge/URI Judge/1043.cpp
@@ -0,0 +1,23 @@
+//#include <iostream>
+//#include <stdio.h>
+//#include <math.h>
+//#include <iomanip>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	float a, b, c, area, perimetro;
+//	cin >> a >> b >> c;
+//
+//	area = (0.5 * (a + b)) * c;
+//	perimetro = a + b + c;
+//
+//	if ((a + b) > c && (b + c) > a && (a + c) > b)
+//		cout << "Perimetro = " << fixed << setprecision(1) << perimetro << endl;
+//
+//	else
+//		cout << "Area = " << fixed << setprecision(1) << area << endl;
+//
+//	return 0;
+//}
