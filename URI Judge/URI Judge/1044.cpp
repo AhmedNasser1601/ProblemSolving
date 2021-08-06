c2cfc469726ba7c4acb7195b57897d21b24a5259
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:31:34 +0200

    Multiples

---
+++ URI Judge/URI Judge/1044.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//#include <stdio.h>
+//#include <math.h>
+//#include <iomanip>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int a, b;
+//
+//	cin >> a >> b;
+//
+//	if (b % a == 0 || a % b == 0)
+//		cout << "Sao Multiplos\n";
+//
+//	else
+//		cout << "Nao sao Multiplos\n";
+//
+//	return 0;
+//}
