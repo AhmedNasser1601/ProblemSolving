130e051573b67c5cdf7c930873165044b234ccce
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:35:52 +0200

    Even or Odd

---
+++ URI Judge/URI Judge/1074.cpp
@@ -0,0 +1,35 @@
+//#include <iostream>
+//#include <iomanip>
+//#include <stdio.h>
+//#include <string>
+//#include <math.h>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int a, b;
+//	cin >> a;
+//
+//	for (int i = 1; i <= a; i++)
+//	{
+//		cin >> b;
+//
+//		if (b % 2 == 0 && b > 0)
+//			cout << "EVEN POSITIVE\n";
+//
+//		if (b % 2 == 0 && b < 0)
+//			cout << "EVEN NEGATIVE\n";
+//
+//		if (b % 2 != 0 && b > 0)
+//			cout << "ODD POSITIVE\n";
+//
+//		if (b % 2 != 0 && b < 0)
+//			cout << "ODD NEGATIVE\n";
+//
+//		if (b == 0)
+//			cout << "NULL\n";
+//	}
+//
+//	return 0;
+//}
