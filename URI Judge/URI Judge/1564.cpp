d28bc9050c444643b272d66c9cddb4da6bfcdc55
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:57:57 +0200

    Brazil World Cup

---
+++ URI Judge/URI Judge/1564.cpp
@@ -0,0 +1,15 @@
+//#include<iostream>
+//
+//using namespace std;
+//
+//int main() {
+//	int x;
+//
+//	while ((cin >> x) && x != EOF) {
+//		if (x) cout << "vai ter duas!\n";
+//
+//		else cout << "vai ter copa!\n";
+//	}
+//
+//	return 0;
+//}
