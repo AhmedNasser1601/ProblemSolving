b6cd062f5a51bbbd4ec62569704e8698d701cccc
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:26:33 +0200

    Simple Calculate

---
+++ URI Judge/URI Judge/1010.cpp
@@ -0,0 +1,17 @@
+//#include<iostream>
+//#include<iomanip>
+//#include<stdio.h>
+//using namespace std;
+//int main()
+//{
+//	int a, b, x, y;
+//	float c, z;
+//	float prod_1, prod_2, total;
+//	cin >> a >> b >> c;
+//	cin >> x >> y >> z;
+//	prod_1 = b * c;
+//	prod_2 = y * z;
+//	total = prod_1 + prod_2;
+//	cout << "VALOR A PAGAR: R$ " << fixed << setprecision(2) << total << endl;
+//	return 0;
+//}
