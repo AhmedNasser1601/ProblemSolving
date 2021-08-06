03d58c096cef298b2e9dc89b3202dd20ca77557d
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:54:55 +0200

    Array Fill II

---
+++ URI Judge/URI Judge/1177.cpp
@@ -0,0 +1,15 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main() {
+//	int x, y[1000];
+//	cin >> x;
+//
+//	for (int i = 0, j = 0; i < 1000; i++, j++) {
+//		if (j == x) j = 0;
+//		cout << "N[" << i << "] = " << j << endl;
+//	}
+//
+//	return 0;
+//}
