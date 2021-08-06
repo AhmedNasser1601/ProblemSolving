d9098ec46d169752df75e2e93e9a9b123be764c7
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:34:17 +0200

    Even Between five Numbers

---
+++ URI Judge/URI Judge/1065.cpp
@@ -0,0 +1,27 @@
+//#include <iostream>
+//#include <iomanip>
+//#include <stdio.h>
+//#include <string>
+//#include <math.h>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int counter = 0;
+//	int x[5] = { 0 };
+//
+//	for (int i = 0; i < 5; i++)
+//	{
+//		cin >> x[i];
+//
+//		if (x[i] % 2 == 0)
+//		{
+//			counter++;
+//		}
+//	}
+//
+//	cout << counter << " valores pares\n";
+//
+//	return 0;
+//}
