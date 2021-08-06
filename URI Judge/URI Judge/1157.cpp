485f61794899dbe481b2e0c005841cbf6e97c2dc
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:51:43 +0200

    Divisors I

---
+++ URI Judge/URI Judge/1157.cpp
@@ -0,0 +1,15 @@
+//#include <iostream>
+//#include <iomanip>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int x; cin >> x;
+//
+//	for (int i = 1; i <= x; i++)
+//		if (x % i == 0)
+//			cout << i << endl;
+//
+//	return 0;
+//}
