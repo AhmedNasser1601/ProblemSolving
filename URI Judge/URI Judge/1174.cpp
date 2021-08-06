fce87d556f390da9012aaecc95b16b913555111f
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:54:19 +0200

    Array Selection I

---
+++ URI Judge/URI Judge/1174.cpp
@@ -0,0 +1,22 @@
+//#include <iostream>
+//#include <iomanip>
+//#include <cmath>
+//#include <cstdio>
+//#include <string>
+//#include <algorithm>
+//#include <vector>
+//
+//using namespace std;
+//
+//int main() {
+//	float arr[100] = {};
+//
+//	for (int i = 0; i < 100; i++)
+//		cin >> arr[i];
+//
+//	for (int i = 0; i < 100; i++)
+//		if (arr[i] <= 10)
+//			cout << "A[" << i << "] = " << fixed << setprecision(1) << arr[i] << endl;
+//
+//	return 0;
+//}
