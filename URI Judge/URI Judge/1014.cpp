69144ca9619737446a2121fcf89aac2529b0f096
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:27:23 +0200

    Consumption

---
+++ URI Judge/URI Judge/1014.cpp
@@ -0,0 +1,11 @@
+//#include<iostream>
+//#include<iomanip>
+//using namespace std;
+//int main()
+//{
+//	int X; float Y, result;
+//	cin >> X >> Y;
+//	result = (X / Y);
+//	cout << fixed << setprecision(3) << result << " km/l" << endl;
+//	return 0;
+//}
