471f8cbe80088930d0f0225e51648aca2e4017ab
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:36:43 +0200

    Weighted Averages

---
+++ URI Judge/URI Judge/1079.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//#include <iomanip>
+//#include <stdio.h>
+//#include <string>
+//#include <math.h>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int N;
+//	float a, s, d;
+//	cin >> N;
+//	for (int i = 1; i <= N; i++)
+//	{
+//		cin >> a >> s >> d;
+//		cout << fixed << setprecision(1) << ((a * 2) + (s * 3) + (d * 5)) / 10 << endl;
+//	}
+//
+//	return 0;
+//}
