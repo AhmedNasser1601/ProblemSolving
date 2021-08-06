cf78f543578bc67372188614cdd26ec26f8f300d
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:37:57 +0200

    Sum of Consecutive Odd Numbers II

---
+++ URI Judge/URI Judge/1099.cpp
@@ -0,0 +1,35 @@
+//#include <iostream>
+//#include <iomanip>
+//#include <stdio.h>
+//#include <string>
+//#include <math.h>
+//
+//using namespace std;
+//
+//int SUM()
+//{
+//	int x, y, sum = 0;
+//	cin >> x >> y;
+//
+//	if (x < y)
+//		for (int i = x + 1; i < y; i++)
+//			if (i % 2 != 0)
+//				sum += i;
+//	if (y < x)
+//		for (int i = y + 1; i < x; i++)
+//			if (i % 2 != 0)
+//				sum += i;
+//
+//	return sum;
+//}
+//
+//int main()
+//{
+//	int N;
+//	cin >> N;
+//
+//	for (int i = 1; i <= N; i++)
+//		cout << SUM() << endl;
+//
+//	return 0;
+//}
