e8d2e27cfb5b6a6ac3f0c27d07117174872291c0
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:34:31 +0200

    Even, Odd, Positive and Negative

---
+++ URI Judge/URI Judge/1066.cpp
@@ -0,0 +1,34 @@
+//#include <iostream>
+//#include <iomanip>
+//#include <stdio.h>
+//#include <string>
+//#include <math.h>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int a = 0, b = 0, c = 0, d = 0;
+//	int x[5] = { 0 };
+//
+//	for (int i = 0; i < 5; i++)
+//	{
+//		cin >> x[i];
+//
+//		if (x[i] % 2 == 0) a++;
+//
+//		if (x[i] % 2 != 0) b++;
+//
+//		if (x[i] > 0) c++;
+//
+//		if (x[i] < 0) d++;
+//	}
+//
+//	cout
+//		<< a << " valor(es) par(es)\n"
+//		<< b << " valor(es) impar(es)\n"
+//		<< c << " valor(es) positivo(s)\n"
+//		<< d << " valor(es) negativo(s)\n";
+//
+//	return 0;
+//}
