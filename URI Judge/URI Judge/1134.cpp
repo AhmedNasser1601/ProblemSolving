73edc3b314f1efeb513d2b35fd860c7862065488
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:49:15 +0200

    Type of Fuel

---
+++ URI Judge/URI Judge/1134.cpp
@@ -0,0 +1,24 @@
+//#include<iostream>
+//using namespace std;
+//int main() {
+//	int x, a = 0, b = 0, c = 0;
+//
+//	while (true) {
+//		cin >> x;
+//
+//		if (x == 1) a++;
+//
+//		if (x == 2) b++;
+//
+//		if (x == 3) c++;
+//
+//		if (x == 4) break;
+//	}
+//
+//	cout << "MUITO OBRIGADO" << endl;
+//	cout << "Alcool: " << a << endl;
+//	cout << "Gasolina: " << b << endl;
+//	cout << "Diesel: " << c << endl;
+//
+//	return 0;
+//}
