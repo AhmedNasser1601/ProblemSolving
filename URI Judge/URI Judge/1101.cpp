4c24b91e8e14e0d414be1cb5f161f49ee0c2187a
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:38:13 +0200

    Sequence of Numbers and Sum

---
+++ URI Judge/URI Judge/1101.cpp
@@ -0,0 +1,36 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//
+//	int n, x, y, aux, soma;
+//
+//	while (true)
+//	{
+//		cin >> x >> y;
+//
+//		if (x <= 0 || y <= 0)
+//			break;
+//
+//		if (x > y)
+//		{
+//			aux = x;
+//			x = y;
+//			y = aux;
+//		}
+//
+//		soma = 0;
+//
+//		for (int i = x; i <= y; i++)
+//		{
+//			cout << i << " ";
+//			soma += i;
+//		}
+//
+//		cout << "Sum=" << soma << endl;
+//	}
+//
+//	return 0;
+//}
