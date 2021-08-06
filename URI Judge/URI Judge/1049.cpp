06ce6715efd2452b20de16bb85169141fedb5f94
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:32:26 +0200

    Animal

---
+++ URI Judge/URI Judge/1049.cpp
@@ -0,0 +1,57 @@
+//#include <iostream>
+//#include <stdio.h>
+//#include <math.h>
+//#include <iomanip>
+//#include <string>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	string x, y, z;
+//	cin >> x >> y >> z;
+//
+//	if (x == "vertebrado")
+//	{
+//		if (y == "ave")
+//		{
+//			if (z == "carnivoro")
+//				cout << "aguia\n";
+//
+//			else if (z == "onivoro")
+//				cout << "pomba\n";
+//		}
+//
+//		else if (y == "mamifero")
+//		{
+//			if (z == "onivoro")
+//				cout << "homem\n";
+//
+//			else if (z == "herbivoro")
+//				cout << "vaca\n";
+//		}
+//	}
+//
+//	else if (x == "invertebrado")
+//	{
+//		if (y == "inseto")
+//		{
+//			if (z == "hematofago")
+//				cout << "pulga\n";
+//
+//			else if (z == "herbivoro")
+//				cout << "lagarta\n";
+//		}
+//
+//		else if (y == "anelideo")
+//		{
+//			if (z == "hematofago")
+//				cout << "sanguessuga\n";
+//
+//			else if (z == "onivoro")
+//				cout << "minhoca\n";
+//		}
+//	}
+//
+//	return 0;
+//}
