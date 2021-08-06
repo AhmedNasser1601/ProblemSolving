fde07ba812d9820eb97a85690e743cbc6967a21f
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:50:28 +0200

    Summing Consecutive Integers

---
+++ URI Judge/URI Judge/1149.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//using namespace std;
+//int main() {
+//	int x, y, z = 0;
+//
+//	while (cin >> x) {
+//		cin >> y;
+//
+//		while (y <= 0) { cin >> y; }
+//
+//		for (int i = 0; i < y; i++) {
+//			z += (x + i);
+//		}
+//
+//		cout << z << endl;
+//		z = 0;
+//	}
+//
+//	return 0;
+//}
