b646d287bfbb6c4a4bb00858b6d6e25681bb0b35
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:53:43 +0200

    Prime Number

---
+++ URI Judge/URI Judge/1165.cpp
@@ -0,0 +1,33 @@
+//#include <iostream>
+//#include <iomanip>
+//#include <stdio.h>
+//#include <string>
+//#include <math.h>
+//
+//using namespace std;
+//
+//int main() {
+//    int n, x, i, j, p = 0;
+//
+//    cin >> n;
+//
+//    for (i = 1; i <= n; i++) {
+//        cin >> x;
+//
+//        for (j = 1, p = 0; j < x; j++) {
+//            if (x % j == 0) {
+//                p += j;
+//            }
+//        }
+//
+//        if (p == 1) {
+//            cout << x << " eh primo\n";
+//        }
+//
+//        else {
+//            cout << x << " nao eh primo\n";
+//        }
+//    }
+//
+//    return 0;
+//}
