8edde880e552b7e3ca30da00c686b7b2d47e4298
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:30:43 +0200

    Snack

---
+++ URI Judge/URI Judge/1038.cpp
@@ -0,0 +1,22 @@
+//#include <iostream>
+//#include <stdio.h>
+//#include <math.h>
+//#include <iomanip>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	float x, y, total;
+//
+//	cin >> x >> y;
+//
+//	float arr[5] = { 4.00, 4.50, 5.00, 2.00, 1.50 };
+//
+//	int index = x - 1;
+//	total = arr[index] * y;
+//
+//	cout << "Total: R$ " << fixed << setprecision(2) << total << endl;
+//
+//	return 0;
+//}
