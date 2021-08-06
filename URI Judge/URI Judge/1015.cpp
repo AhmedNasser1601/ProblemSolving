f2b873ca858fd9fab351a88a07865a4fce47fef3
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:27:37 +0200

    Distance Between Two Points

---
+++ URI Judge/URI Judge/1015.cpp
@@ -0,0 +1,12 @@
+//#include<iostream>
+//#include<iomanip>
+//#include<math.h>
+//using namespace std;
+//int main()
+//{
+//	double x1, y1, x2, y2, dist;
+//	cin >> x1 >> y1 >> x2 >> y2;
+//	dist = sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+//	cout << fixed << setprecision(4) << dist << endl;
+//	return 0;
+//}
