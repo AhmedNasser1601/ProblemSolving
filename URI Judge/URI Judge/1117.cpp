f25eaae2d2bfd14eef07165ec24f189358b58775
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:47:59 +0200

    Score Validation

---
+++ URI Judge/URI Judge/1117.cpp
@@ -0,0 +1,33 @@
+//#include <iostream>
+//#include <iomanip>
+//#include <stdio.h>
+//#include <cstdio>
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	float x = 0, y = 0, z;
+//
+//	while (x == 0 || y == 0)
+//	{
+//		cin >> z;
+//
+//		if (z >= 0 && z <= 10 && x != 0)
+//		{
+//			y = z;
+//			cout << "media = " << ((x + y) / 2) << endl;
+//			break;
+//		}
+//
+//		if (z >= 0 && z <= 10)
+//			x = z;
+//
+//		else
+//			cout << "nota invalida\n";
+//	}
+//
+//	return 0;
+//}
