3d9523b34f9e8ee15a4650ff1e16ed1c804556c0
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:29:35 +0200

    Bhaskara's Formula

---
+++ URI Judge/URI Judge/1036.cpp
@@ -0,0 +1,27 @@
+//#include <iostream>
+//#include <stdio.h>
+//#include <math.h>
+//#include <iomanip>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	double a, b, c, t;
+//
+//	cin >> a >> b >> c;
+//
+//	if (((b * b) - 4 * a * c) < 0 || a == 0)
+//		cout << "Impossivel calcular\n";
+//
+//	else
+//	{
+//		t = sqrt((b * b) - 4 * a * c);
+//
+//		cout
+//			<< "R1 = " << fixed << setprecision(5) << ((-b + t) / (2 * a)) << endl
+//			<< "R2 = " << fixed << setprecision(5) << ((-b - t) / (2 * a)) << endl;
+//	}
+//
+//	return 0;
+//}
