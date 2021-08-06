3845e5e9a231fdce957b2bde4a904724bec346c6
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:55:25 +0200

    Lowest Number and Position

---
+++ URI Judge/URI Judge/1180.cpp
@@ -0,0 +1,27 @@
+//#include <iostream>
+//#include <iomanip>
+//#include <string>
+//#include <cstdio>
+//#include <cmath>
+//
+//using namespace std;
+//
+//int main() {
+//	int num; cin >> num;
+//	int* arr = new int[num];
+//
+//	int min = 9999, pos;
+//
+//	for (int i = 0; i < num; i++) {
+//		cin >> arr[i];
+//
+//		if (arr[i] < min) {
+//			min = arr[i];
+//			pos = i;
+//		}
+//	}
+//
+//	cout << "Menor valor: " << min << "\nPosicao: " << pos << endl;
+//
+//	return 0;
+//}
