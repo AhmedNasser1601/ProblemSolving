c054cd4c81fb6dae32f02e5fbcae24df6390454f
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:26:57 +0200

    Area

---
+++ URI Judge/URI Judge/1012.cpp
@@ -0,0 +1,23 @@
+//#include<iostream>
+//#include<iomanip>
+//#include<stdio.h>
+//using namespace std;
+//int main()
+//{
+//	float A, B, C;
+//	cin >> A >> B >> C;
+//	double triangle, circulo, trapezio, quadrado, retangulo, pi = 3.14159;
+//
+//	triangle = 0.5 * A * C;
+//	circulo = pi * C * C;
+//	trapezio = 0.5 * (A + B) * C;
+//	quadrado = B * B;
+//	retangulo = A * B;
+//
+//	cout << "TRIANGULO: " << fixed << setprecision(3) << triangle << endl;
+//	cout << "CIRCULO: " << fixed << setprecision(3) << circulo << endl;
+//	cout << "TRAPEZIO: " << fixed << setprecision(3) << trapezio << endl;
+//	cout << "QUADRADO: " << fixed << setprecision(3) << quadrado << endl;
+//	cout << "RETANGULO: " << fixed << setprecision(3) << retangulo << endl;
+//	return 0;
+//}
