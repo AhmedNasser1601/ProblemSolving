b17a700af86040037c7ae611e59810b5c8b1bdb4
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:31:03 +0200

    Coordinates of a Point

---
+++ URI Judge/URI Judge/1041.cpp
@@ -0,0 +1,29 @@
+//#include <iostream>
+//#include <stdio.h>
+//#include <math.h>
+//#include <iomanip>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	float x, y;
+//	cin >> x >> y;
+//
+//	if (x > 0 && y > 0)
+//		cout << "Q1\n";
+//	else if (x < 0 && y>0)
+//		cout << "Q2\n";
+//	else if (x < 0 && y < 0)
+//		cout << "Q3\n";
+//	else if (x > 0 && y < 0)
+//		cout << "Q4\n";
+//	else if (x != 0 && y == 0)
+//		cout << "Eixo X\n";
+//	else if (x == 0 && y != 0)
+//		cout << "Eixo Y\n";
+//	else
+//		cout << "Origem\n";
+//
+//	return 0;
+//}
