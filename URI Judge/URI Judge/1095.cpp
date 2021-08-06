948bc295e7069d1bf6b582e955b9f187fb6daa09
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:37:18 +0200

    Sequence IJ 1

---
+++ URI Judge/URI Judge/1095.cpp
@@ -0,0 +1,25 @@
+//#include <iostream>
+//#include <iomanip>
+//#include <stdio.h>
+//#include <string>
+//#include <math.h>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int i = 1, k;
+//
+//	do
+//	{
+//		for (int j = 60; j >= 0; j = j - 5)
+//		{
+//			cout << "I=" << i << " " << "J=" << j << endl;
+//
+//			i = i + 3;
+//			k = j;
+//		}
+//	} while (k < 0);
+//
+//	return 0;
+//}
