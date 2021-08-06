07d6f1f318eb09b8bdbad7bfb7c38ca5a16bee76
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:58:25 +0200

    Bill

---
+++ URI Judge/URI Judge/1866.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main() {
+//	int num, x;
+//	cin >> x;
+//
+//	for (int i = 0; i < x; i++) {
+//		cin >> num;
+//
+//		if (num % 2 == 0)
+//			cout << 0 << endl;
+//
+//		else
+//			cout << 1 << endl;
+//	}
+//
+//	return 0;
+//}
