b7cd8790c109c435806bf675dc36ebc4abe224b5
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:36:56 +0200

    Highest and Position

---
+++ URI Judge/URI Judge/1080.cpp
@@ -0,0 +1,14 @@
+//#include <iostream>
+//using namespace std;
+//int main() {
+//	int x[100], max = 0, index = 0;
+//	for (int i = 0; i < 100; i++) {
+//		cin >> x[i];
+//		if (x[i] > max) {
+//			max = x[i];
+//			index = i + 1;
+//		}
+//	}
+//	cout << max << endl << index << endl;
+//	return 0;
+//}
