e805240baa18b09d9c51ca790403488bee0cb79b
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:31:55 +0200

    Game Time

---
+++ URI Judge/URI Judge/1046.cpp
@@ -0,0 +1,24 @@
+//#include <iostream>
+//#include <stdio.h>
+//#include <math.h>
+//#include <iomanip>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int x, y, sub;
+//	cin >> x >> y;
+//
+//	if (x >= 0 && y >= 0)
+//	{
+//		sub = y - x + 24;
+//
+//		if (sub > 24)
+//			sub = sub - 24;
+//
+//		cout << "O JOGO DUROU " << sub << " HORA(S)\n";
+//	}
+//
+//	return 0;
+//}
