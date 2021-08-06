96c8c12cc2ed10f138d51cf95674a5488b7ff3d8
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:50:00 +0200

    Logical Sequence 2

---
+++ URI Judge/URI Judge/1145.cpp
@@ -0,0 +1,25 @@
+//#include<iostream>
+//
+//using namespace std;
+//
+//int main() {
+//	int x, y;
+//	cin >> x >> y;
+//
+//	int j = 0;
+//
+//	for (int i = 1; i <= y; i++) {
+//		j++;
+//		cout << i;
+//
+//		if (j == x || i == y) {
+//			cout << "\n";
+//			j = 0;
+//		}
+//
+//		else
+//			cout << " ";
+//	}
+//
+//	return 0;
+//}
