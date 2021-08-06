bfd956c71e9b6fe63b6fd4eba9ed4215c071f9e8
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:48:35 +0200

    Multiples of 13

---
+++ URI Judge/URI Judge/1132.cpp
@@ -0,0 +1,22 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main() {
+//	int x, y, z = 0;
+//	cin >> x >> y;
+//
+//	if (x < y)
+//		for (; x <= y; x++)
+//			if (x % 13 != 0)
+//				z += x;
+//
+//	if (x > y)
+//		for (; y <= x; y++)
+//			if (y % 13 != 0)
+//				z += y;
+//
+//	cout << z << endl;
+//
+//	return 0;
+//}
