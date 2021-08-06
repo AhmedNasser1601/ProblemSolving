a9ed3a7b122ac19d412bea685025fe3cfb91c20e
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:26:09 +0200

    Salary

---
+++ URI Judge/URI Judge/1008.cpp
@@ -0,0 +1,11 @@
+//#include <iostream>
+//#include<iomanip>
+//using namespace std;
+//int main() {
+//	int A, B; float C, D;
+//	cin >> A >> B >> C;
+//	D = B * C;
+//	cout << "NUMBER = " << A << endl;
+//	cout << "SALARY = U$ " << fixed << setprecision(2) << D << endl;
+//	return 0;
+//}
