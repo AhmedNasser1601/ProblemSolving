3b517c7e7bb75b7ce36a2a3bb3812d317f3a9566
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:27:47 +0200

    Distance

---
+++ URI Judge/URI Judge/1016.cpp
@@ -0,0 +1,21 @@
+//#include<iostream>
+//#include<iomanip>
+//#include<math.h>
+//using namespace std;
+//int main()
+//{
+//	int km, min;
+//	cin >> km;
+//	if (km <= 30)
+//	{
+//		min = km * 2;
+//		cout << min << " minutos" << endl;
+//	}
+//	if (km > 30)
+//	{
+//		int temp = km - 30;
+//		min = 60 + temp * 2;
+//		cout << min << " minutos" << endl;
+//	}
+//	return 0;
+//}
