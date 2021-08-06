972d1945d1872c87653390da4095dce6fedab5f3
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:33:45 +0200

    Positive Numbers

---
+++ URI Judge/URI Judge/1060.cpp
@@ -0,0 +1,25 @@
+//#include <iostream>
+//#include <iomanip>
+//#include <stdio.h>
+//#include <string>
+//#include <math.h>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	float arr[6];
+//	int counter = 0;
+//
+//	for (int i = 0; i < 6; i++)
+//	{
+//		cin >> arr[i];
+//
+//		if (arr[i] > 0)
+//			counter++;
+//	}
+//
+//	cout << counter << " valores positivos\n";
+//
+//	return 0;
+//}
