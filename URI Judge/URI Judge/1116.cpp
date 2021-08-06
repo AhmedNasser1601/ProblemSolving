b9912579ade121437cf0b05fa11cf500ce090029
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:47:49 +0200

    Dividing X by Y

---
+++ URI Judge/URI Judge/1116.cpp
@@ -0,0 +1,28 @@
+//#include <iostream>
+//#include <iomanip>
+//#include <stdio.h>
+//#include <cstdio>
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int size; cin >> size;
+//
+//	double x, y;
+//
+//	for (int i = 0; i < size; i++)
+//	{
+//		cin >> x >> y;
+//
+//		if (y == 0)
+//			cout << "divisao impossivel\n";
+//
+//		else
+//			cout << fixed << setprecision(1) << x / y << endl;
+//	}
+//
+//	return 0;
+//}
