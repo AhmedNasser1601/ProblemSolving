a64aefaf64fb8f2e8dc2f2963d65730600aa10e0
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:35:37 +0200

    Even Square

---
+++ URI Judge/URI Judge/1073.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//#include <iomanip>
+//#include <stdio.h>
+//#include <string>
+//#include <math.h>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int N;
+//	cin >> N;
+//
+//	for (int i = 2; i <= N; i++)
+//	{
+//		if (i % 2 == 0)
+//			cout << i << "^" << "2 = " << i * i << endl;
+//	}
+//
+//	return 0;
+//}
