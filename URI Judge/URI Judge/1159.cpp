812b6c84fb35ee39bef11a43d409c5209d938f33
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:52:04 +0200

    Sum of Consecutive Even Numbers

---
+++ URI Judge/URI Judge/1159.cpp
@@ -0,0 +1,30 @@
+//#include <iostream>
+//#include <iomanip>
+//#include <stdio.h>
+//#include <string>
+//#include <math.h>
+//#include <string.h>
+//
+//using namespace std;
+//
+//int main() {
+//	int x, y;
+//
+//	while (cin >> x) {
+//		y = 0;
+//
+//		if (x == 0) {
+//			break;
+//		}
+//
+//		for (int i = x; i < x + 10; i++) {
+//			if (i % 2 == 0) {
+//				y += i;
+//			}
+//		}
+//
+//		cout << y << endl;
+//	}
+//
+//	return 0;
+//}
