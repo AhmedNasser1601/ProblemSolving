5f840ea7ae964f3b104b5eac74e945329aaa30d5
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:28:43 +0200

    Banknotes and Coins

---
+++ URI Judge/URI Judge/1021.cpp
@@ -0,0 +1,55 @@
+//#include <iostream>
+//#include <iomanip>
+//#include <stdio.h>
+//
+//using namespace std;
+//int main()
+//{
+//	int n100, n50, n20, n10, n5, n2, m1, m50, m25, m10, m05, m01;
+//
+//	double x; cin >> x;
+//
+//	int y = x, z = (x - y) * 100;
+//
+//	n100 = y / 100;
+//	y = y % 100;
+//	n50 = y / 50;
+//	y = y % 50;
+//	n20 = y / 20;
+//	y = y % 20;
+//	n10 = y / 10;
+//	y = y % 10;
+//	n5 = y / 5;
+//	y = y % 5;
+//	n2 = y / 2;
+//	y = y % 2;
+//
+//	m1 = y / 1;
+//	y = y % 1;
+//	m50 = z / 50;
+//	z = z % 50;
+//	m25 = z / 25;
+//	z = z % 25;
+//	m10 = z / 10;
+//	z = z % 10;
+//	m05 = z / 5;
+//	z = z % 5;
+//	m01 = z / 1;
+//
+//	cout << "NOTAS:\n"
+//		<< n100 << " nota(s) de R$ 100.00\n"
+//		<< n50 << " nota(s) de R$ 50.00\n"
+//		<< n20 << " nota(s) de R$ 20.00\n"
+//		<< n10 << " nota(s) de R$ 10.00\n"
+//		<< n5 << " nota(s) de R$ 5.00\n"
+//		<< n2 << " nota(s) de R$ 2.00\n"
+//		<< "MOEDAS:\n"
+//		<< m1 << " moeda(s) de R$ 1.00\n"
+//		<< m50 << " moeda(s) de R$ 0.50\n"
+//		<< m25 << " moeda(s) de R$ 0.25\n"
+//		<< m10 << " moeda(s) de R$ 0.10\n"
+//		<< m05 << " moeda(s) de R$ 0.05\n"
+//		<< m01 << " moeda(s) de R$ 0.01\n";
+//
+//	return 0;
+//}
