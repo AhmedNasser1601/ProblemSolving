7d12d4388d18f262a9ab0c6b959ecfed63e4aded
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:54:31 +0200

    Array change I

---
+++ URI Judge/URI Judge/1175.cpp
@@ -0,0 +1,30 @@
+//#include <iostream>
+//#include <iomanip>
+//#include <cmath>
+//#include <cstdio>
+//#include <string>
+//#include <algorithm>
+//#include <vector>
+//
+//using namespace std;
+//
+//int main() {
+//	int arr[20] = {}, temp;
+//
+//	for (int i = 0; i < 20; i++)
+//		cin >> arr[i];
+//
+//	for (int i = 0, j = 19; i < 20, j >= 0; i++, j--) {
+//		if (i == 10 || j == 9)
+//			break;
+//
+//		temp = arr[i];
+//		arr[i] = arr[j];
+//		arr[j] = temp;
+//	}
+//
+//	for (int i = 0; i < 20; i++)
+//		cout << "N[" << i << "] = " << arr[i] << endl;
+//
+//	return 0;
+//}
