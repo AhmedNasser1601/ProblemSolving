8551c037fba26bf0aa5f0f222786b722a1e12fc3
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:56:42 +0200

    Below the Secundary Diagonal

---
+++ URI Judge/URI Judge/1186.cpp
@@ -0,0 +1,28 @@
+//#include <iostream>
+//#include <iomanip>
+//
+//using namespace std;
+//
+//int main() {
+//	float M[12][12], SUM = 0;
+//	char operation;
+//
+//	cin >> operation;
+//
+//	for (int i = 0; i < 12; i++) {
+//		for (int j = 0; j < 12; j++) {
+//			cin >> M[i][j];
+//
+//			if (i + j > 11)
+//				SUM += M[i][j];
+//		}
+//	}
+//
+//	if (operation == 'S')
+//		cout << fixed << setprecision(1) << SUM << endl;
+//
+//	else if (operation == 'M')
+//		cout << fixed << setprecision(1) << (SUM / 66) << endl;
+//
+//	return 0;
+//}
