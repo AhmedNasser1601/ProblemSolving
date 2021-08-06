b375f51ad0fa884b42abecde71f2ba3fecaa839f
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:59:28 +0200

    Rest 1.0

---
+++ URI Judge/URI Judge/3091.cpp
@@ -0,0 +1,17 @@
+//#include <iostream>
+//#include <iomanip>
+//#include <stdio.h>
+//#include <string>
+//#include <math.h>
+//#include <string.h>
+//
+//using namespace std;
+//
+//int main() {
+//	int x, y, z = 0;
+//	cin >> x >> y;
+//
+//	cout << (x % y) << endl;
+//
+//	return 0;
+//}
