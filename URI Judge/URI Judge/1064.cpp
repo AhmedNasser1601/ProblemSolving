0d72cf92dc2a4b6ac601da55803d7aea031ede5b
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:34:07 +0200

    Positives and Average

---
+++ URI Judge/URI Judge/1064.cpp
@@ -0,0 +1,31 @@
+//#include <iostream>
+//#include <iomanip>
+//#include <stdio.h>
+//#include <string>
+//#include <math.h>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int counter = 0; float average = 0;
+//	float x[6] = { 0 };
+//
+//	for (int i = 0; i < 6; i++)
+//	{
+//		cin >> x[i];
+//
+//		if (x[i] > 0)
+//		{
+//			counter++;
+//			average += x[i];
+//		}
+//	}
+//
+//	average = average / counter;
+//
+//	cout << counter << " valores positivos\n";
+//	cout << fixed << setprecision(1) << average << endl;
+//
+//	return 0;
+//}
