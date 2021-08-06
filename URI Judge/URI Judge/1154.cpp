e588e4b9612f461534a07144aab77af68709ac3b
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:51:06 +0200

    Ages

---
+++ URI Judge/URI Judge/1154.cpp
@@ -0,0 +1,25 @@
+//#include <iostream>
+//#include <stdio.h>
+//#include <iomanip>
+//#include <string>
+//
+//using namespace std;
+//
+//int main() {
+//	int x = 0, y = -1;
+//	float z = 0;
+//
+//	while (x >= 0)
+//	{
+//		cin >> x;
+//
+//		if (x >= 0)
+//			z += x;
+//
+//		y++;
+//	}
+//
+//	cout << fixed << setprecision(2) << z / y << endl;
+//
+//	return 0;
+//}
