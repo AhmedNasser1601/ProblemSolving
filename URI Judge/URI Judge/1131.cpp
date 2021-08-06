d507eaaae28270a4e2399806d7c2f4e7d72530c8
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:48:22 +0200

    Grenais

---
+++ URI Judge/URI Judge/1131.cpp
@@ -0,0 +1,52 @@
+//#include <iostream>
+//#include <iomanip>
+//#include <stdio.h>
+//#include <cstdio>
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//int main() {
+//	int x, y, a, b, c, d;
+//	a = b = c = d = 0;
+//
+//	while (true)
+//	{
+//		cin >> x >> y;
+//		d++;
+//
+//		if (x == y)
+//			c++;
+//
+//		else if (x > y)
+//			a++;
+//
+//		else if (x < y)
+//			b++;
+//
+//		cout << "Novo grenal (1-sim 2-nao)\n";
+//
+//		cin >> x;
+//
+//		if (x == 2)
+//			break;
+//	}
+//
+//	cout
+//		<< d << " grenais" << endl
+//		<< "Inter:" << a << endl
+//		<< "Gremio:" << b << endl
+//		<< "Empates:" << c << endl;
+//
+//	if (a == b)
+//		cout << "Nao houve vencedor\n";
+//
+//	else if (a > b)
+//		cout << "Inter venceu mais\n";
+//
+//	else if (a < b)
+//		cout << "Gremio venceu mais\n";
+//
+//	return 0;
+//}
