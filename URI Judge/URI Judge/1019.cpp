9a64c9362b80c143c4fe37e7ab85cd5fdbe6ab1e
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:28:20 +0200

    Time Conversion

---
+++ URI Judge/URI Judge/1019.cpp
@@ -0,0 +1,29 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int num;
+//	cin >> num;
+//
+//	int h = 0, m = 0, s = 0;
+//
+//	s = num;
+//
+//	do
+//	{
+//		if (s >= 60)
+//		{
+//			m = m + 1;
+//			s = s - 60;
+//		}
+//
+//		if (m >= 60)
+//		{
+//			h = h + 1;
+//			m = m - 60;
+//		}
+//	} while (s >= 60);
+//
+//	cout << h << ":" << m << ":" << s << endl;
+//	return 0;
+//}
