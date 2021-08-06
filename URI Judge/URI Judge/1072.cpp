0dab3fb01c0e6c1dc9f9486b4a3255c498661d99
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:35:26 +0200

    Interval 2

---
+++ URI Judge/URI Judge/1072.cpp
@@ -0,0 +1,28 @@
+//#include <iostream>
+//#include <iomanip>
+//#include <stdio.h>
+//#include <string>
+//#include <math.h>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int x, a, count_in = 0, count_out = 0;
+//	cin >> x;
+//
+//	for (int i = 0; i < x; i++)
+//	{
+//		cin >> a;
+//
+//		if (a >= 10 && a <= 20)
+//			count_in++;
+//
+//		else
+//			count_out++;
+//	}
+//
+//	cout << count_in << " in\n" << count_out << " out\n";
+//
+//	return 0;
+//}
