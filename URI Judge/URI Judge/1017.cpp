3c961b933150a56590267d27fbc402725fc1ced4
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:27:55 +0200

    Fuel Spent

---
+++ URI Judge/URI Judge/1017.cpp
@@ -0,0 +1,12 @@
+//#include<iostream>
+//#include<iomanip>
+//#include<math.h>
+//#include<cstdio>
+//using namespace std;
+//int main()
+//{
+//	double t, s;
+//	cin >> t >> s;
+//	cout << fixed << setprecision(3) << (t * s) / 12 << endl;
+//	return 0;
+//}
