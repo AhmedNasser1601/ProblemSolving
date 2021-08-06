eaf949b28480dc67cf09b1ab05030cc307c5f0f2
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:49:35 +0200

    Squared and Cubic

---
+++ URI Judge/URI Judge/1143.cpp
@@ -0,0 +1,12 @@
+//#include <iostream>
+//using namespace std;
+//int main() {
+//	int x, j, k;
+//	cin >> x;
+//	for (int i = 1; i <= x; i++) {
+//		j = i * i;
+//		k = i * i * i;
+//		cout << i << " " << j << " " << k << endl;
+//	}
+//	return 0;
+//}
