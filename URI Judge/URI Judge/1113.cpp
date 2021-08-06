4ec2d28fd3e8defb4efc2a9c76b3c07f36c3453e
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:47:11 +0200

    Ascending and Descending

---
+++ URI Judge/URI Judge/1113.cpp
@@ -0,0 +1,18 @@
+//#include<iostream>
+//
+//using namespace std;
+//
+//int main() {
+//	while (true) {
+//		int x, y;
+//		cin >> x >> y;
+//
+//		if (x < y) cout << "Crescente\n";
+//
+//		else if (x > y) cout << "Decrescente\n";
+//
+//		else break;
+//	}
+//
+//	return 0;
+//}
