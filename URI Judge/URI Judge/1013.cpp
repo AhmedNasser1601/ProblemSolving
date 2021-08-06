1620b1938afd00851466475b99cb9c245ac67061
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:27:12 +0200

    The Greatest

---
+++ URI Judge/URI Judge/1013.cpp
@@ -0,0 +1,11 @@
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int a, b, c, AB, maior;
+//	cin >> a >> b >> c;
+//	AB = (a + b + abs(a - b)) / 2;
+//	maior = (AB + c + abs(AB - c)) / 2;
+//	cout << maior << " eh o maior" << endl;
+//	return 0;
+//}
