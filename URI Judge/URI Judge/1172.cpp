c0dd317a974b970937517a8eddd46d9eb6e72dd7
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:53:55 +0200

    Array Replacement I

---
+++ URI Judge/URI Judge/1172.cpp
@@ -0,0 +1,25 @@
+//#include <iostream>
+//#include <iomanip>
+//#include <cmath>
+//#include <cstdio>
+//#include <string>
+//#include <algorithm>
+//#include <vector>
+//
+//using namespace std;
+//
+//int main() {
+//	int arr[10] = {};
+//
+//	for (int i = 0; i < 10; i++) {
+//		cin >> arr[i];
+//
+//		if (arr[i] <= 0)
+//			arr[i] = 1;
+//	}
+//
+//	for (int i = 0; i < 10; i++)
+//		cout << "X[" << i << "] = " << arr[i] << endl;
+//
+//	return 0;
+//}
