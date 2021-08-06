ce8eb7f32c05359debc7437994d625a3905593eb
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:49:44 +0200

    Logical Sequence

---
+++ URI Judge/URI Judge/1144.cpp
@@ -0,0 +1,13 @@
+//#include<iostream>
+//using namespace std;
+//int main() {
+//	int x;
+//	cin >> x;
+//
+//	for (int j = 1; j <= x; j++) {
+//		cout << j << " " << j * j << " " << j * j * j << endl;
+//		cout << j << " " << (j * j) + 1 << " " << (j * j * j) + 1 << endl;
+//	}
+//
+//	return 0;
+//}
