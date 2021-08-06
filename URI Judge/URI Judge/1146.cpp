9148abda6b0e650251f76b4b5dd1c7cbd10c0498
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:50:15 +0200

    Growing Sequences

---
+++ URI Judge/URI Judge/1146.cpp
@@ -0,0 +1,25 @@
+//#include<iostream>
+//
+//using namespace std;
+//
+//int main() {
+//	int x;
+//
+//	do {
+//		cin >> x;
+//
+//		if (x == 0) break;
+//
+//		for (int i = 1; i <= x; i++) {
+//			cout << i;
+//
+//			if (i == x)
+//				cout << endl;
+//
+//			else
+//				cout << " ";
+//		}
+//	} while (true);
+//
+//	return 0;
+//}
