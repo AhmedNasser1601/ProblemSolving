9cc2adf580bc83053690c56bddc667eb38d22a98
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:33:10 +0200

    Taxes

---
+++ URI Judge/URI Judge/1051.cpp
@@ -0,0 +1,22 @@
+//#include<iostream>
+//#include<iomanip>
+//#include<string>
+//using namespace std;
+//int main() {
+//	float x;
+//	cin >> x;
+//
+//	if (x > 0.00 && x <= 2000.00)
+//		cout << "Isento" << endl;
+//
+//	else if (x > 2000.00 && x <= 3000.00)
+//		cout << "R$ " << fixed << setprecision(2) << ((x - 2000.00) * 0.08) << endl;
+//
+//	else if (x > 3000 && x <= 4500)
+//		cout << "R$ " << fixed << setprecision(2) << ((x - 3000.00) * 0.18) + (1000.00 * 0.08) << endl;
+//
+//	else if (x > 4500)
+//		cout << "R$ " << fixed << setprecision(2) << ((x - 4500.00) * 0.28) + (1000.00 * 0.08) + (1500.00 * 0.18) << endl;
+//
+//	return 0;
+//}
