adbaf4ce27a800f43b979c522f65f1c7d8dd4157
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:36:11 +0200

    Multiplication Table

---
+++ URI Judge/URI Judge/1078.cpp
@@ -0,0 +1,19 @@
+//#include <iostream>
+//#include <iomanip>
+//#include <stdio.h>
+//#include <string>
+//#include <math.h>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int N = 0;
+//	cin >> N;
+//
+//	if (N > 2 && N < 1000)
+//		for (int i = 1; i < 11; i++)
+//			cout << i << " x " << N << " = " << i * N << endl;
+//
+//	return 0;
+//}
