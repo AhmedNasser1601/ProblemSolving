bff477a1d494132dbd22a711ecdd8ed0e7889d02
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:26:47 +0200

    Sphere

---
+++ URI Judge/URI Judge/1011.cpp
@@ -0,0 +1,13 @@
+//#include<iostream>
+//#include<iomanip>
+//#include<stdio.h>
+//using namespace std;
+//int main()
+//{
+//	double pi = 3.14159, result;
+//	int R;
+//	cin >> R;
+//	result = (4 / 3.0) * pi * R * R * R;
+//	cout << "VOLUME = " << fixed << setprecision(3) << result << endl;
+//	return 0;
+//}
