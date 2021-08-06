4da7d16ef75263e2cefcb37ad2a66fb7e2f93ca0
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:57:46 +0200

    Array 123

---
+++ URI Judge/URI Judge/1534.cpp
@@ -0,0 +1,25 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main() {
+//	int x;
+//
+//	while (cin >> x && x != EOF) {
+//		for (int i = 0; i < x; i++) {
+//			for (int j = 0; j < x; j++) {
+//				if (j == (x - 1) - i)
+//					cout << 2;
+//
+//				else if (i == j)
+//					cout << 1;
+//
+//				else
+//					cout << 3;
+//			}
+//			cout << endl;
+//		}
+//	}
+//
+//	return 0;
+//}
