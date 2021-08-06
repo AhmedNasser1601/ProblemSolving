59555d0d188b033529d2ff47f127aa49ef98ba52
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:53:26 +0200

    Factorial Sum

---
+++ URI Judge/URI Judge/1161.cpp
@@ -0,0 +1,32 @@
+//#include <iostream>
+//#include <iomanip>
+//#include <stdio.h>
+//#include <string>
+//#include <math.h>
+//
+//using namespace std;
+//
+//int main() {
+//	long long int a, b, x = 1, y = 1;
+//
+//	while (cin >> a >> b)
+//	{
+//		x = y = 1;
+//
+//		for (int i = a; i > 0; --i)
+//		{
+//			x *= a;
+//			a--;
+//		}
+//
+//		for (int i = b; i > 0; --i)
+//		{
+//			y *= b;
+//			b--;
+//		}
+//
+//		cout << (x + y) << endl;
+//	}
+//
+//	return 0;
+//}
