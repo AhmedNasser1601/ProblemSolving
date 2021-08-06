6e31f3c69322cb5b1523727d02f483054abbb812
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:51:21 +0200

    S Sequence

---
+++ URI Judge/URI Judge/1155.cpp
@@ -0,0 +1,17 @@
+//#include <iostream>
+//#include <stdio.h>
+//#include <iomanip>
+//#include <string>
+//
+//using namespace std;
+//
+//int main() {
+//	float x = 0;
+//
+//	for (float i = 1; i <= 100; ++i)
+//		x += 1 / i;
+//
+//	cout << fixed << setprecision(2) << x << endl;
+//
+//	return 0;
+//}
