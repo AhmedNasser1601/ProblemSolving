cc0cd6d7726c7189ce468cd23b7f5f47880a397e
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:38:34 +0200

    Fixed Password

---
+++ URI Judge/URI Judge/1114.cpp
@@ -0,0 +1,30 @@
+//#include <iostream>
+//#include <stdio.h>
+//#include <cstdio>
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int num = 2002;
+//	int input; cin >> input;
+//
+//	while (true)
+//	{
+//		if (input != num)
+//		{
+//			cout << "Senha Invalida\n";
+//			cin >> input;
+//		}
+//
+//		if (input == num)
+//		{
+//			cout << "Acesso Permitido\n";
+//			break;
+//		}
+//	}
+//
+//	return 0;
+//}
