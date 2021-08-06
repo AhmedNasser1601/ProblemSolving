528ef4be63d24baf524ebbeab22b088243029233
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:34:57 +0200

    Six Odd Numbers

---
+++ URI Judge/URI Judge/1070.cpp
@@ -0,0 +1,22 @@
+//#include <iostream>
+//#include <iomanip>
+//#include <stdio.h>
+//#include <string>
+//#include <math.h>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int x; cin >> x;
+//
+//	for (int i = x; i <= x + 11;)
+//	{
+//		if (i % 2 != 0)
+//			cout << i << endl;
+//
+//		i++;
+//	}
+//
+//	return 0;
+//}
