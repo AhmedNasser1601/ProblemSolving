989556c1335fb1847214cf22d13f8d8d73e90847
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:37:38 +0200

    Sequence IJ 3

---
+++ URI Judge/URI Judge/1097.cpp
@@ -0,0 +1,29 @@
+//#include <iostream>
+//#include <iomanip>
+//#include <stdio.h>
+//#include <string>
+//#include <math.h>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int i = -1;
+//
+//	do
+//	{
+//		for (int j = 7; j <= 15; j = j + 2)
+//		{
+//			i = i + 2;
+//
+//			cout << "I=" << i << " " << "J=" << j << endl;
+//			cout << "I=" << i << " " << "J=" << j - 1 << endl;
+//			cout << "I=" << i << " " << "J=" << j - 2 << endl;
+//
+//			if (j == j - 2)
+//				j = j + 4;
+//		}
+//	} while (i > 9);
+//
+//	return 0;
+//}
