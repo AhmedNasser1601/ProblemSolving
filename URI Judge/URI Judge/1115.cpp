797181ef4dc0b657ee857ef1f6bb71e2b8a2d951
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:47:39 +0200

    Quadrant

---
+++ URI Judge/URI Judge/1115.cpp
@@ -0,0 +1,29 @@
+//#include <iostream>
+//#include <stdio.h>
+//#include <cstdio>
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int x, y;
+//
+//	do
+//	{
+//		cin >> x >> y;
+//
+//		if (x > 0 && y > 0)
+//			cout << "primeiro\n";
+//		if (x < 0 && y > 0)
+//			cout << "segundo\n";
+//		if (x < 0 && y < 0)
+//			cout << "terceiro\n";
+//		if (x > 0 && y < 0)
+//			cout << "quarto\n";
+//
+//	} while (x != 0 || y != 0);
+//
+//	return 0;
+//}
