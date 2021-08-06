0fe6c77a27fb194bf5f768ba58087aa72ebc79bf
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:50:59 +0200

    Simple Factorial

---
+++ URI Judge/URI Judge/1153.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//#include <stdio.h>
+//#include <iomanip>
+//#include <string>
+//
+//using namespace std;
+//
+//int main() {
+//	int x, y = 1; cin >> x;
+//
+//	if (x > 0 && x < 13) {
+//		for (; x >= 1; x--) {
+//			y *= x;
+//		}
+//	}
+//
+//	cout << y << endl;
+//
+//	return 0;
+//}
