31509612e9d53e39ee9ddf35b36b825d588536ca
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:49:24 +0200

    PUM

---
+++ URI Judge/URI Judge/1142.cpp
@@ -0,0 +1,15 @@
+//#include<iostream>
+//#include<iomanip>
+//using namespace std;
+//int main() {
+//	int x; cin >> x;
+//
+//	for (int i = 1; i < (4 * x - 1); i += 4) {
+//		for (int j = i; j <= i + 2; j++)
+//			cout << j << " ";
+//
+//		cout << "PUM" << endl;
+//	}
+//
+//	return 0;
+//}
