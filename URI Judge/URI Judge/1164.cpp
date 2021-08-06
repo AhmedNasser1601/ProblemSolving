a9b9ae49ef7a4d1ada55a19c7287ab1f477ecc51
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-08-06 17:52:48 +0200

    Perfect Number

---
+++ URI Judge/URI Judge/1164.cpp
@@ -0,0 +1,35 @@
+//#include <iostream>
+//#include <iomanip>
+//#include <stdio.h>
+//#include <string>
+//#include <math.h>
+//#include <string.h>
+//
+//using namespace std;
+//
+//int main() {
+//    int x, y, z = 0;
+//    cin >> x;
+//
+//    for (int i = 0; i < x; i++) {
+//        cin >> y;
+//
+//        z = 0;
+//
+//        for (int j = y - 1; j >= 1; j--) {
+//            if (y % j == 0) {
+//                z += j;
+//            }
+//        }
+//
+//        if (z == y) {
+//            cout << y << " eh perfeito\n";
+//        }
+//
+//        else {
+//            cout << y << " nao eh perfeito\n";
+//        }
+//    }
+//
+//    return 0;
+//}
