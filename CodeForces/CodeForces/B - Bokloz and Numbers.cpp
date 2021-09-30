554dc3a671de0dcdaa404a2bb1eb83e61ecb6f7d
Ahmed Nasser <ahmed20191701016@cis.asu.edu.eg>
2021-09-30 12:54:21 +0200

    Week 2: Practice, problem: (B) Bokloz and Numbers

---
+++ CodeForces/CodeForces/B - Bokloz and Numbers.cpp
@@ -0,0 +1,19 @@
+//#include<iostream>
+//
+//using namespace std;
+//
+//int main() {
+//	int x, max = 0, tmp;
+//	cin >> x;
+//
+//	for (int i = 0; i < x; i++) {
+//		cin >> tmp;
+//
+//		if (tmp > max)
+//			max = tmp;
+//	}
+//
+//	cout << max << endl;
+//
+//	return 0;
+//}
